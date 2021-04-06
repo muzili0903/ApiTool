5884cbff7c56398e0f90f330d47a113c78f6705c
muzili0903 <2584167983@qq.com>
2021-04-06 21:56:47 +0800

    user register logging

--- user/logic.py
+++ user/logic.py
@@ -6,7 +6,8 @@ def user_is_exist(user):
         User.objects.get(user=user)
     except User.DoesNotExist:
         return False
-    return True
+    finally:
+        return True
 
 
 def password_is_identical(password, pwd):
